b8027f4de0a7413cfd770539792734368d081ebd
anhnthe176694 <tuananhxgvp1@gmail.com>
2025-02-08 15:45:25 +0700

    feat:DatabaseMigrations

--- another/src/main/java/com/sep490_backend/authentication/core/ApiCode.java
+++
@@ -1,7 +0,0 @@
-package com.sep490_backend.authentication.core;
-
-public interface ApiCode {
-    String getMessage();
-
-    Integer getCode();
-}
---
+++ operation/src/test/java/com/sep490backend/operation/OperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep490backend.operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
